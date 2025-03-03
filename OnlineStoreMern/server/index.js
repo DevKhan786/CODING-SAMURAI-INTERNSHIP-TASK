0defb6cdc81a823686c8e75fdda24e0a690365f8
DevVain <InsaneVain786@gmail.com>
2025-03-03 15:49:30 +0000

    new

--- OnlineStoreMern/server/index.js
+++ OnlineStoreMern/server/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-app.use(cors({
-  credentials: true,
-  origin: 
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  })
+);
